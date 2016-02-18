0a08a7d5a1b6f8bc5973fa128c9e38a76ec92b22
cong luat <congluat27@gmail.com>
2016-02-18 15:16:58 -0500

    121`31241241

---
+++ src/main/java/model/Invoice.java
@@ -0,0 +1,12 @@
+package model;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+public class Invoice implements Serializable{
+	
+	private Integer id;
+	private String name;
+	private BigDecimal amount;
+	
+}
