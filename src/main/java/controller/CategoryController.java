b526060a2fdb786ed2834d56d499214ef6061857
quoclinhit <quoclinhit@gmail.com>
2016-02-23 13:31:50 +0700

    123456

--- src/main/java/controller/CategoryController.java
+++ src/main/java/controller/CategoryController.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -28,52 +27,33 @@ public class CategoryController {
 	@Autowired
 	@Qualifier("categoryService")
 	CategoryService cateService;
-
+	
 	@Autowired
 	ServletContext application;
 
 	@RequestMapping(value = "/getAllCategories", method = RequestMethod.GET)
 	@ResponseBody
 	public String getAllCategories() {
-		
-		
 		String json = new Gson().toJson(cateService.getAllCategories());
 		return json;
 	}
-
-	@RequestMapping(value = "/listCategories", method = RequestMethod.GET)
-	public String listCategories(ModelMap model) {
-		model.addAttribute("categories", cateService.getAllCategories());
-		model.addAttribute("title", "Category");
-		return "category";
-	}
-
-	@RequestMapping(value = "/edit/{id}", method = RequestMethod.GET)
-	public String editCategoryById(@PathVariable Integer id, ModelMap model) {
-		Category cate = cateService.getById(id);
-		model.addAttribute("category", cate);
-//		System.out.println("Cate:" + cate.getId() + cate.getName() + cate.getLogo() + cate.getDescription());
-//		String json = new Gson().toJson("Xin chào các bạn");
-		
-		
-		return "save-cate";
-	}
-
-	@RequestMapping(value = "/save", method = RequestMethod.GET)
+	
+	@RequestMapping(value="/save", method = RequestMethod.GET)
 	public String InsertCate(ModelMap model) {
 		Category category = new Category();
 
 		model.addAttribute("category", category);
-		return "save-cate";
+		return "category";
 	}
-
-	@RequestMapping(value = "/save", method = RequestMethod.POST)
-	public String Save(@ModelAttribute Category category, @RequestParam("file") MultipartFile file, ModelMap model) {
+	
+	@RequestMapping(value="/save", method = RequestMethod.POST)
+	public String Save(@ModelAttribute Category category, @RequestParam("file") MultipartFile file,
+							ModelMap model) {
 		String logo = file.getOriginalFilename();
 		category.setLogo("abc.png");
-
+		
 		try {
-			String path = application.getRealPath("resources/logo/") + logo;
+			String path = application.getRealPath("resources/images/category/") + logo;
 			if (!logo.equals("")) {
 				file.transferTo(new File(path));
 				category.setLogo(path);
@@ -81,12 +61,12 @@ public class CategoryController {
 		} catch (Exception e) {
 			category.setLogo("abc.png");
 		}
-
+		
 		cateService.create(category);
-
+		
 		model.addAttribute("category", new Category());
 		model.addAttribute("message", "save category success!");
-
-		return "save-cate";
+		
+		return "category";
 	}
 }
